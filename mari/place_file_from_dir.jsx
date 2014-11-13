6da54db2dcf32bcd8f6d23eee59a487df64a8cdd
Johan Borgström <johan@Johans-Mac-Pro.local>
2014-11-13 10:37:57 +0100

    add add_to_udim function

--- mari/place_file_from_dir.jsx
+++ mari/place_file_from_dir.jsx
@@ -1,24 +1,55 @@
 // minified json: https://github.com/douglascrockford/JSON-js/blob/master/json2.js
 //if(typeof JSON!=="object"){JSON={}}(function(){"use strict";function f(e){return e<10?"0"+e:e}function quote(e){escapable.lastIndex=0;return escapable.test(e)?'"'+e.replace(escapable,function(e){var t=meta[e];return typeof t==="string"?t:"\\u"+("0000"+e.charCodeAt(0).toString(16)).slice(-4)})+'"':'"'+e+'"'}function str(e,t){var n,r,i,s,o=gap,u,a=t[e];if(a&&typeof a==="object"&&typeof a.toJSON==="function"){a=a.toJSON(e)}if(typeof rep==="function"){a=rep.call(t,e,a)}switch(typeof a){case"string":return quote(a);case"number":return isFinite(a)?String(a):"null";case"boolean":case"null":return String(a);case"object":if(!a){return"null"}gap+=indent;u=[];if(Object.prototype.toString.apply(a)==="[object Array]"){s=a.length;for(n=0;n<s;n+=1){u[n]=str(n,a)||"null"}i=u.length===0?"[]":gap?"[\n"+gap+u.join(",\n"+gap)+"\n"+o+"]":"["+u.join(",")+"]";gap=o;return i}if(rep&&typeof rep==="object"){s=rep.length;for(n=0;n<s;n+=1){if(typeof rep[n]==="string"){r=rep[n];i=str(r,a);if(i){u.push(quote(r)+(gap?": ":":")+i)}}}}else{for(r in a){if(Object.prototype.hasOwnProperty.call(a,r)){i=str(r,a);if(i){u.push(quote(r)+(gap?": ":":")+i)}}}}i=u.length===0?"{}":gap?"{\n"+gap+u.join(",\n"+gap)+"\n"+o+"}":"{"+u.join(",")+"}";gap=o;return i}}if(typeof Date.prototype.toJSON!=="function"){Date.prototype.toJSON=function(){return isFinite(this.valueOf())?this.getUTCFullYear()+"-"+f(this.getUTCMonth()+1)+"-"+f(this.getUTCDate())+"T"+f(this.getUTCHours())+":"+f(this.getUTCMinutes())+":"+f(this.getUTCSeconds())+"Z":null};String.prototype.toJSON=Number.prototype.toJSON=Boolean.prototype.toJSON=function(){return this.valueOf()}}var cx,escapable,gap,indent,meta,rep;if(typeof JSON.stringify!=="function"){escapable=/[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;meta={"\b":"\\b","  ":"\\t","\n":"\\n","\f":"\\f","\r":"\\r",'"':'\\"',"\\":"\\\\"};JSON.stringify=function(e,t,n){var r;gap="";indent="";if(typeof n==="number"){for(r=0;r<n;r+=1){indent+=" "}}else if(typeof n==="string"){indent=n}rep=t;if(t&&typeof t!=="function"&&(typeof t!=="object"||typeof t.length!=="number")){throw new Error("JSON.stringify")}return str("",{"":e})}}if(typeof JSON.parse!=="function"){cx=/[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;JSON.parse=function(text,reviver){function walk(e,t){var n,r,i=e[t];if(i&&typeof i==="object"){for(n in i){if(Object.prototype.hasOwnProperty.call(i,n)){r=walk(i,n);if(r!==undefined){i[n]=r}else{delete i[n]}}}}return reviver.call(e,t,i)}var j;text=String(text);cx.lastIndex=0;if(cx.test(text)){text=text.replace(cx,function(e){return"\\u"+("0000"+e.charCodeAt(0).toString(16)).slice(-4)})}if(/^[\],:{}\s]*$/.test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g,"@").replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,"]").replace(/(?:^|:|,)(?:\s*\[)+/g,""))){j=eval("("+text+")");return typeof reviver==="function"?walk({"":j},""):j}throw new SyntaxError("JSON.parse")}}})()
 
-// open a new blank document
-var doc = app.documents.add(); 
-
-// the default layer
-var default_layer = doc.layers[0];
-default_layer.name = "udim_ref";
-
-// open the UDIM dir
-dir = Folder.selectDialog('Please select the folder to be imported:', Folder('~/Documents/projects/the_bot/bot_illustrator/'));
-
 var num_col = 10;
 var tile_size = 1024;
 var start_x = -7000;
 var start_y = -5000;
 
-var inc = 0;
-if (dir)
+function create_new_udim_document()
 {
+    // open a new blank document
+    var active_doc = app.documents.add(); 
+
+    // the default layer
+    var default_layer = active_doc.layers[0];
+
+    // open the UDIM dir
+    dir = Folder.selectDialog('Please select the folder to be imported:', Folder('~/Documents/projects/the_bot/bot_illustrator/'));
+    
+    if (dir)
+    {
+        create_udim_tiles(doc=active_doc, create_artboard=true, add_graphics_layer=true)
+    }
+
+    // If we have more than one artboard, remove the first
+    if (active_doc.artboards.length > 1)
+    {
+        active_doc.artboards.remove(0); 
+    }
+
+    if (active_doc.layers.length > 1)
+    {
+        default_layer.remove(); 
+    }
+}
+
+function add_to_udim_document()
+{
+    // open the UDIM dir
+    dir = Folder.selectDialog('Please select the folder to be imported:', Folder('~/Documents/projects/the_bot/bot_illustrator/'));
+
+    if (dir)
+    {
+        var active_doc = app.activeDocument;
+        create_udim_tiles(doc=active_doc, create_artboard=false, add_graphics_layer=false)
+    }   
+}
+function create_udim_tiles(doc, create_artboard, add_graphics_layer)
+{
+    var udim_layer = doc.layers.add();
+    udim_layer.name = "udim";
+    
     var img_list = dir.getFiles();
     var num_img = img_list.length;
     //num_img = 2;
@@ -41,10 +72,11 @@ if (dir)
         
         // get the udim
         udim = last_part.slice(last_part.length-4, last_part.length);
-        $.writeln(udim);
+        var inc = udim - 1001;
+        $.writeln(inc);
    
         // Place the image on the artboard
-        var placed_img = default_layer.placedItems.add()
+        var placed_img = udim_layer.placedItems.add()
         placed_img.file = img_list[i];
         
         var left = (inc%num_col)*tile_size;
@@ -52,22 +84,29 @@ if (dir)
         placed_img.left = start_x + left;
         placed_img.top = start_y + top;
         placed_img.width = placed_img.height = tile_size;
-        
+        /*
         for(x in placed_img)
         {
             $.writeln(x)
         }
-        bounds = placed_img.geometricBounds;
-        var art_board = doc.artboards.add(bounds);
-        art_board.name = udim;
-
-        // only increment if the file was not skipped
-        inc++;
+        */
+        if(create_artboard)
+        {
+            bounds = placed_img.geometricBounds;
+            var art_board = doc.artboards.add(bounds);
+            art_board.name = udim;      
+        }  
     }
 
-    // delete default artboard
-    doc.artboards.remove(0); 
-    default_layer.locked = true;
-    var new_layer = doc.layers.add();
-    new_layer.name = "awesome graphics";
+    udim_layer.locked = true;
+    
+    if(add_graphics_layer)
+    {
+        var new_layer = doc.layers.add();
+        new_layer.name = "awesome graphics";
+    }  
 }
+
+
+create_new_udim_document();
+//add_to_udim_document();
